a125c2a0d4ecbe3759986bea4582b2b6206353be
rjmcbain <rmcbain01@gmail.com>
2021-02-17 11:08:46 -0800

    added js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,94 @@ const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
-function getRandomUser() {
-  fetch(`https://randomuser.me/api`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.results[0].gender);
-    });
+let data = []; // Initializing Array
+
+// Fetch random user and add money
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+async function getRandomUser() {
+  const res = await fetch(`https://randomuser.me/api`); // Getting data from api
+  const data = await res.json();
+
+  const user = data.results[0]; // Assigning results to user
+
+  const newUser = {
+    // creating object with results
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  addData(newUser);
+}
+
+// Double money
+
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
+// Show Millionaires
+
+function showMillionaires() {
+  data = data.filter((user) => {
+    return user.money > 1000000;
+  });
+
+  updateDOM();
+}
+
+// Sort by richest
+
+function sortByRichest() {
+  data.sort((a, b) => {
+    return b.money - a.money;
+  });
+  updateDOM();
+}
+
+// Calculate total wealth
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+// Add new obj to data array
+function addData(obj) {
+  data.push(obj); // pushing object into data array
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
+
+  providedData.forEach((item) => {
+    // Looping through data array
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+function formatMoney(number) {
+  // Formatting money
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 }
 
 // Event listeners
