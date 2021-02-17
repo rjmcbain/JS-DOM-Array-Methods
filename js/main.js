599d2f58ab91f49f9c3642e38db54f0b25cecfc6
rjmcbain <rmcbain01@gmail.com>
2021-02-17 10:17:23 -0800

    adding html and css

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+function getRandomUser() {
+  fetch(`https://randomuser.me/api`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.results[0].gender);
+    });
+}
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
